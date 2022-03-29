5cfd79f62be9c85dd4ee9a44d95cf9d3c6dfc0db
tyrocca <tyler.rocca@gmail.com>
2022-03-29 09:58:27 -0400

    wip

---
+++ internal/models/baserequests.go
@@ -0,0 +1,13 @@
+package models
+
+// Use for querying a table
+type Query struct{}
+
+// Use for Updating a db object
+type CreateParams struct{}
+
+// use for updating objects
+type UpdateParams struct{}
+
+// use for deleting
+type DeleteParams struct{}
